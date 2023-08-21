8228d0ba5b33b147faedfc258fed3b54ee463c04
DanielProgramming56 <chideberedanieldevelopement@gmail.com>
2023-08-21 15:50:04 +0100

    message

--- src/layout/shoppingCart.jsx
+++ src/layout/shoppingCart.jsx
@@ -1,10 +1,21 @@
 import React,{} from "react"
 
-const ShoppingCart = () => {
-
+const ShoppingCart = ({cartItems}) => {
+	const noCart = cartItems.length < 1
+	console.log(cartItems)
 	return(
 
-		<h1>Hello World</h1>
+		<div className="shoppingCart">
+		{noCart && <h1>No Cart</h1>} 
+
+		<div className="singleCart">
+			{cartItems && cartItems.map((data) => <div key={data.id}>
+				<img  src={data.image} alt={data.id} />
+				<h1>{data.title}</h1>
+				<h3>{data.price}</h3>
+			</div>)}
+		</div>
+		</div>
 	)
 }
 
